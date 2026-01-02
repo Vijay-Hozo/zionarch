c6eb858284a93272a3516cd059462a3a6404bfda
Vijay-Hozo <vijay2304a@gmail.com>
2026-01-02 12:57:41 +0530

    cors error

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:8081',
+  origin: process.env.FRONTEND_URL || 'http://localhost:8081' || 'http://zionarch.vercel.app' || '*',
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
